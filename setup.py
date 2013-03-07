54a2ac524e0b092eda1204ba9006a8e591070c68
Damilare Onajole <web.dami@gmail.com>
2013-03-07 15:01:52 +0000

    adding setup.py for inclusion in PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PythonExacttarget',
+    version='0.1.0',
+    author='Matthew Black',
+    author_email='consult@jbadigital.com',
+    packages=find_packages(),
+    url='https://github.com/damilare/python-exacttarget/',
+    license='LICENSE.txt',
+    description='Python Wrapper for Exact Target SOAP API',
+    long_description='Python Wrapper for Exact Target SOAP API',
+)
